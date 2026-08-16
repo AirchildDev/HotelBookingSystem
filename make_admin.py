958a69cc71fe9b5df1aa994788defe9f167893a9
AirchildDev <ekeomaonuoha2000@gmail.com>
2026-08-16 14:09:22 +0100

    Fix automated tests and test discovery

--- make_admin.py
+++ make_admin.py
@@ -1,13 +1,15 @@
 import sqlite3
 
+username = input("Enter username to make admin: ")
+
 con = sqlite3.connect("hotel.db")
 cur = con.cursor()
 
 cur.execute("""
 UPDATE users
 SET role='admin'
-WHERE username='komzy'
-""")
+WHERE username=?
+""", (username,))
 
 con.commit()
 con.close()
